adf514db3dfd47c114a082e8dc448bfb10d0eb19
dugun319 <110073858+dugun319@users.noreply.github.com>
2024-05-22 11:38:04 +0900

    Add files via upload

--- ch_02_0522/src/ch_02_0522/GarbageValueExample.java
+++ ch_02_0522/src/ch_02_0522/GarbageValueExample.java
@@ -3,7 +3,15 @@ package ch_02_0522;
 public class GarbageValueExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		byte var1 = 125;
+		int var2 = 125;
+		
+		//      1.초기값 2.비교 3.증분
+		for(int i = 0 ; i<5 ; i++) {
+			var1++;
+			var2++;
+			System.out.println("var1: " + var1 + "\t" + "var2: " + var2);
+		}
 
 	}
 
