a03f05597605f8d4cf6a9c9b32496fea754aa8c8
dugun319 <110073858+dugun319@users.noreply.github.com>
2024-05-23 12:24:10 +0900

    Add files via upload

---
+++ ch_02_0522/src/ch_02_ex02/ConcatenationExample.java
@@ -0,0 +1,15 @@
+package ch_02_ex02;
+
+public class ConcatenationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "num = " + 3 + 4;
+		String str2 = 3 + 4 + " = num";
+		System.out.println("str1 -> " + str1);
+		System.out.println("str2 -> " + str2);
+		
+
+	}
+
+}
