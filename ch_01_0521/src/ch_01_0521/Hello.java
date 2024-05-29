e346c3852823992644c84e048874610dc48812e2
dugun319 <110073858+dugun319@users.noreply.github.com>
2024-05-29 17:47:14 +0900

    Add files via upload

---
+++ ch_01_0521/src/ch_01_0521/Hello.java
@@ -0,0 +1,18 @@
+package ch_01_0521;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 7;
+		
+		String A = "Today is Tuesday~";
+		 
+		System.out.println(a+b);
+
+		System.out.println(A);
+		
+		System.out.println("Hello, Java World!");
+	}
+
+}
---
+++ ch_01_0521/src/ch_01_0521/hello2.java
@@ -0,0 +1,8 @@
+package ch_01_0521;
+
+public class hello2 {
+	public static void main(String[] args) {
+		System.out.println("두번째");
+	}
+
+}
